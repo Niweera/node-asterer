d5f026d800626441e0d2225c65f80c83f3b349ce
Nipuna Weerasekara <w.nipuna@gmail.com>
2020-12-24 10:45:22 +0530

    add new tests

--- bin/checkInputs/checkInputs.spec.js
+++ bin/checkInputs/checkInputs.spec.js
@@ -18,10 +18,51 @@ describe("check inputs [input_path, output_path, config_path]", () => {
   });
 
   it("should check the input path, the output path and the config path", async () => {
-    try {
-      await checkInputs(input_path_, output_path_, config_path_);
-    } catch (e) {
-      console.log(e);
-    }
+    expect.assertions(1);
+    return expect(
+      checkInputs(input_path_, output_path_, config_path_)
+    ).resolves.toEqual(undefined);
+  });
+
+  it("should check the null config path", async () => {
+    expect.assertions(1);
+    return expect(
+      checkInputs(input_path_, output_path_, null)
+    ).resolves.toEqual(undefined);
+  });
+
+  it("should check the invalid config path", async () => {
+    expect.assertions(1);
+    await expect(
+      checkInputs(input_path_, output_path_, "invalid_config_path")
+    ).rejects.toEqual("invalid_config_path does not exist.");
+  });
+
+  it("should check a valid config path but the file is not a json", async () => {
+    expect.assertions(1);
+    await expect(
+      checkInputs(input_path_, output_path_, input_path_)
+    ).rejects.toEqual(`${input_path_} is not a JSON file.`);
+  });
+
+  it("should check invalid input path", async () => {
+    expect.assertions(1);
+    await expect(
+      checkInputs("input_path_", output_path_, config_path_)
+    ).rejects.toEqual(`input_path_ does not exist.`);
+  });
+
+  it("should check a valid input path but not a JavaScript file", async () => {
+    expect.assertions(1);
+    await expect(
+      checkInputs(config_path_, output_path_, config_path_)
+    ).rejects.toEqual(`${config_path_} is not a JavaScript file.`);
+  });
+
+  it("should check if the output path already exists", async () => {
+    expect.assertions(1);
+    await expect(
+      checkInputs(input_path_, input_path_, config_path_)
+    ).rejects.toEqual(`${input_path_} is already existing.`);
   });
 });
--- bin/getConfig/getConfig.spec.js
+++ bin/getConfig/getConfig.spec.js
@@ -12,6 +12,7 @@ describe("get config object", () => {
 
   it("should get the config object", async () => {
     try {
+      jest.unmock("./getConfig");
       const json_object = await getJSON(config_path_);
       const local_json_object = await getConfig(null);
       expect(json_object).toStrictEqual({ ecmaVersion: 2019, locations: true });
@@ -22,5 +23,12 @@ describe("get config object", () => {
     } catch (e) {
       console.log(e);
     }
+  });
+
+  it("should check if the output path already exists", async () => {
+    expect.assertions(1);
+    await expect(getJSON("config_path_")).rejects.toEqual(
+      `config_path_ does not exist.`
+    );
   });
 });
